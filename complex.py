85f4cd146e14cceb5f4c890b4807cebf40c60128
Keshav Gupta <guptakeshav0710@gmail.com>
2022-12-19 23:48:56 +0530

    Add files via upload

---
+++ complex.py
@@ -0,0 +1,15 @@
+import cmath
+
+class comp:
+
+    def add(self,z1,z2):
+        add=z1+z2
+        print("Addition is:",add)
+
+    def substract(self,z1,z2):
+        sub=z1-z2
+        print("Substraction is:",sub)
+
+    def multiply(self,z1,z2):
+        mul=z1*z2
+        print("Multipication is:",mul)
